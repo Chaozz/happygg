bd15e363a142c2198f976bde5547929be3d3b67e
Chaoz <chaoz@chaodeMacBook-Pro.local>
2016-08-25 22:17:53 -0400

    fix some bugs

--- src_tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chaoz/Documents/project/ happygg_github/happygg/src_tests
-# Build directory: /Users/chaoz/Documents/project/ happygg_github/happygg/src_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib/googletest)
-subdirs(basic_tests)
